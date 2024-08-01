086920259b51f2ced09be77b85706bc5bd77b9a0
Zhang <2751628691@qq.com>
2024-08-01 14:52:08 +0800

    feat: springboot 项目模版初始化

---
+++ src/main/java/com/zjz/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.zjz.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zjz.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
